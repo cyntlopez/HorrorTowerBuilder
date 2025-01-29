e0ecaa31ea71c5673e62746047aa35a764d0580f
Cynthia Lopez <cynt.lopez.5@gmail.com>
2025-01-28 18:31:55 -0800

    Added Cabin Image and Cabin Class

--- main.js
+++ main.js
@@ -3,13 +3,17 @@ const gameEngine = new GameEngine();
 const ASSET_MANAGER = new AssetManager();
 
 ASSET_MANAGER.queueDownload("assets/hero/hero_walking.png");
+ASSET_MANAGER.queueDownload("assets/landscape/cabin.png");
 
 ASSET_MANAGER.downloadAll(() => {
     const canvas = document.getElementById("gameWorld");
     const ctx = canvas.getContext("2d");
 
-    const heroSpritesheet = ASSET_MANAGER.getAsset("assets/hero/hero_walking.png");
-    gameEngine.addEntity(new Hero(gameEngine, 10, 10, heroSpritesheet));
+    const heroWalking = ASSET_MANAGER.getAsset("assets/hero/hero_walking.png");
+    gameEngine.addEntity(new Hero(gameEngine, 50, 50, heroWalking));
+
+    const cabin = ASSET_MANAGER.getAsset("assets/landscape/cabin.png");
+    gameEngine.addEntity(new Cabin(gameEngine, 200, 500, cabin));
 
     gameEngine.init(ctx);
 
